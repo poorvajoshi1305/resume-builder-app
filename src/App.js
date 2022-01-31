72d809dfea02ce3ef7dedb7634a8ff2a369a4275
Poorva Joshi <poorvajoshi1305@gmail.com>
2022-01-31 09:01:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
 import React from "react";
-// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-// import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import { Switch, Route, Redirect } from "react-router-dom";
-import Home from "./Home";
-import Login from "./Login";
-import UserProfile from "./UserProfile";
-import UsersData from "./UsersData";
-import UserEducation from "./UserEducation";
-import UserExperience from "./UserExperience";
-// import SignUp from "./signup";
-// import SignIn from "./signin";
-import Register from "./Register";
-import ContactUs from "./ContactUs";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import Resume from "./Resume";
-// import SignIn from "./SignIn";
-// import Users from './Users';
-// import Messages from './Messages';
-
+import Admin from "./Admin/Admin.js";
+import SignUp from "./Pages/SignUp";
+import SignIn from "./Pages/SignIn";
+import UserEducation from "./Pages/UserEducation";
+import UserExperience from "./Pages/UserExperience";
+import UserProfile from "./Pages/UserProfile.js";
+import Resume from "./Pages/Resume.js";
+import Register from "./Register_Login/Register.js";
+import Login from "./Register_Login/Login.js";
+import Landing from "./Components/Landing.js";
+import Home from "./Components/Home.js";
+import Footer from "./Components/Footer.js";
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
+      <Route exact path="/landing" component={Landing} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/" component={Home} />
+      
+      <Route exact path="/admin" component={Admin} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/signin" component={SignIn} />
         <Route exact path="/userprofile" component={UserProfile} />
-        <Route exact path="/usersdata" component={UsersData} />
         <Route exact path="/usereducation" component={UserEducation} />
         <Route exact path="/userexperience" component={UserExperience} />
         <Route exact path="/resume" component={Resume} />
-        {/* <Route exact path="/signup" component={SignUp} /> */}
-        {/* <Route exact path="/signin" component={SignIn} /> */}
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/contactus" component={ContactUs} />
-        {/* <Route exact path="/users" component={Users} /> */}
-        {/* <Route exact path="/messages" component={Messages} /> */}
-        {/* <Route exact path="/visitors" component={Visitors} /> */}
         <Redirect to="/" />
       </Switch>
       <Footer />
